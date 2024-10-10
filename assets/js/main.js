e0a0419d148b0def2efbc5b59a2bc5d0db60a055
Jacqueline Gomez <137355862+JaGo-1@users.noreply.github.com>
2024-10-10 15:19:19 -0300

    Cambios y carrousel agregado

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const imagenes = [
+  "/assets/img/banner_1.webp",
+  "/assets/img/banner_2.png",
+  "/assets/img/banner_3.png",
+];
+let i = 0;
+let img = document.getElementById("main__carousel-img");
+const nextBtn = document.getElementById("btn-next");
+const previousBtn = document.getElementById("btn-previous");
+
+img.src = imagenes[i];
+
+function next() {
+  i++;
+  if (i >= imagenes.length) {
+    i = 0;
+  }
+  img.src = imagenes[i];
+}
+
+function previous() {
+  i--;
+  if (i < 0) {
+    i = imagenes.length - 1;
+  }
+  img.src = imagenes[i];
+}
+
+nextBtn.addEventListener("click", next);
+previousBtn.addEventListener("click", previous);
+setInterval(next, 6000);
